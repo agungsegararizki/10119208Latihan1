24278cb84e5d074daf4e8a2b26094bcf46313c97
agungsegararizki <agungsegara99@gmail.com>
2022-04-16 15:06:23 +0700

    first commit

---
+++ app/src/main/java/com/example/a10119208latihan1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a10119208latihan1;
+
+//15 april 2022, 10119208 Agung Segara Rizki IF5
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
